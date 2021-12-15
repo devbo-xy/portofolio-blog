5b770b45154e388660f93b28aa3e5ef2d5906c53
hanifakbari <91877601+hanifakbari@users.noreply.github.com>
2021-12-15 09:44:51 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ import Logo from '../assets/Logo.svg'
 import Mail from '../assets/mail.svg'
 import Aos from 'aos'
 import 'aos/dist/aos.css'
-import HTML from '../assets/html.svg'
+import Skill from '../component/Skills'
 
 function Home() {
 
@@ -62,113 +62,8 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <div id="skills" className="min-h-screen bg-io dark:bg-darkfirst w-full py-20">
-                <div className="text-center">
-                    <button data-aos="fade-top" data-aos-duration="1000" className="text-text dark:text-white px-5 py-3 border-6 border-text uppercase text-xl md:text-3xl font-bold">
-                        Skills
-                    </button >
-                    <p className="mt-5 mx-6 text-lg md:text-xl font-medium">
-                        Knowledge is not skill. Knowledge plus ten thousand time is skill
-                    </p>
-                </div>
-                <div className="max-w-3xl mx-auto my-auto px-10 md:mt-12 mt-5 md:px-2 py-5">
-                    <div className="flex justify-start ml-5 mb-5">
-                        <h1 className="uppercase font-bold text-xl">using now :</h1>
-                    </div>
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
-                        <div className="w-full flex flex-col justify-center items-center">
-                            <a href="https://en.wikipedia.org/wiki/HTML" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal uppercase">html5</h1>
-                            </div>
-                        </div>
-                        <div className="w-full flex flex-col justify-center items-center">
-                            <a href="https://en.wikipedia.org/wiki/CSS" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal uppercase">css5</h1>
-                            </div>
-                        </div>
-                        <div className="w-full flex flex-col justify-center items-center">
-                            <a href="https://www.figma.com/" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal">Figma</h1>
-                            </div>
-                        </div>
-                        <div className="w-full flex flex-col justify-center items-center">
-                            <a href="https://www.mysql.com/" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal uppercase">MySQL</h1>
-                            </div>
-                        </div>
-                        <div className="w-full flex flex-col justify-center items-center mt-5">
-                            <a href="https://id.wikipedia.org/wiki/Visual_Basic" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal">Visual Basic</h1>
-                            </div>
-                        </div>
-                        <div className="w-full flex flex-col justify-center items-center mt-5">
-                            <a href="https://en.wikipedia.org/wiki/C%2B%2B" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal uppercase">c++</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-start ml-5 mt-10 mb-5">
-                        <h1 className="uppercase font-bold text-xl">learning :</h1>
-                    </div>
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
-                        <div className="w-full flex flex-col justify-center items-center">
-                            <a href="https://en.wikipedia.org/wiki/JavaScript" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal">Javascript</h1>
-                            </div>
-                        </div>
-                        <div className="w-full flex flex-col justify-center items-center">
-                            <a href="https://en.wikipedia.org/wiki/React_(JavaScript_library)" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal">ReactJS</h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex justify-start ml-5 mt-10 mb-5">
-                        <h1 className="uppercase font-bold text-xl">other skills :</h1>
-                    </div>
-                    <div className="grid grid-cols-2 md:grid-cols-4 gap-2">
-                        <div className="w-full flex flex-col justify-center items-center">
-                            <a href="https://en.wikipedia.org/wiki/JavaScript" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal">Microsoft Oracle</h1>
-                            </div>
-                        </div>
-                        <div className="w-full flex flex-col justify-center items-center">
-                            <a href="https://en.wikipedia.org/wiki/React_(JavaScript_library)" className="w-full h-full flex justify-center" >
-                                <img src={HTML} alt="" className="w-24 h-24"></img>
-                            </a>
-                            <div className="text-center">
-                                <h1 className="text-xl mt-2 font-normal">AdobeXD</h1>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Skill />
+
         </Fragment>
     )
 }
